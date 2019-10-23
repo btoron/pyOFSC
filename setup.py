7be864f012f54d7a531746012bd800755c6d6466
Borja Toron <borja.toron@gmail.com>
2019-10-23 11:41:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pyOFSC',         # How you named your package folder (MyLib)
   packages = ['pyOFSC'],   # Chose the same as "name"
-  version = '1.1',      # Start with a small number and increase it with every change you make
+  version = 'v1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python Wrapper for Oracle Field Service Cloud API',   # Give a short description about your library
   author = 'Borja Toron',                   # Type in your name
   author_email = 'borja.toron@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/btoron/pyOFSC',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/btoron/pyOFSC/archive/1.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/btoron/pyOFSC/archive/v1.2.tar.gz',    # I explain this later on
   keywords = ['OFSC', 'Python', 'ORACLE FIELD SERVICE CLOUD'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
