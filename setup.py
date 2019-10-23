3f3bfac86b3e6d36cfb2755a05e7a0a645530c72
Borja Toron <borja.toron@gmail.com>
2019-10-23 11:36:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
   download_url = 'https://github.com/btoron/pyOFSC/archive/1.0.tar.gz',    # I explain this later on
   keywords = ['OFSC', 'Python', 'ORACLE FIELD SERVICE CLOUD'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'requests',
+          'base64',
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
