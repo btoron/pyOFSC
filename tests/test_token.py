2512d306370ad932c7db4f1f2c233973b313be72
Borja Toron <borja.toron@gmail.com>
2023-07-31 08:26:01 -0400

    Adjust testing

--- tests/test_token.py
+++ tests/test_token.py
@@ -11,11 +11,11 @@ from ofsc.common import FULL_RESPONSE, JSON_RESPONSE, TEXT_RESPONSE
 
 @pytest.mark.debug
 def test_get_token(instance, request_logging):
-    logging.info("...302: Get Subscriptions using token")
+    logging.info("...401: Get token")
     raw_response = instance.oauth2.get_token(response_type=FULL_RESPONSE)
     print(raw_response.content)
     assert raw_response.status_code == 200
     response = raw_response.json()
     assert "access_token" in response.keys()
     assert response["token_type"] == "bearer"
-    assert response["expires_in"] == "3600"
+    assert response["expires_in"] == 3600
