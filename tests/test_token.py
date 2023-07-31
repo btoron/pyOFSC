2263bb12e244c344d0e3bd773dd32570ce17a619
Borja Toron <borja.toron@gmail.com>
2023-07-31 09:14:06 -0400

    Test adjustment

--- tests/test_token.py
+++ tests/test_token.py
@@ -3,7 +3,7 @@ import logging
 from ofsc.common import FULL_RESPONSE
 
 
-def test_get_token(instance_with_token, request_logging):
+def test_get_token(instance_with_token):
     logging.info("...401: Get token")
     raw_response = instance_with_token.oauth2.get_token(response_type=FULL_RESPONSE)
     print(raw_response)
@@ -14,7 +14,7 @@ def test_get_token(instance_with_token, request_logging):
     assert response["expires_in"] == 3600
 
 
-def test_cache_token(instance_with_token, request_logging):
+def test_cache_token(instance_with_token):
     logging.info("...402: Verify token cache")
     raw_response = instance_with_token.oauth2.get_token(response_type=FULL_RESPONSE)
     assert raw_response.status_code == 200
